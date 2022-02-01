742da0632a2320242b70c6e4ddc9a58d451e8eee
renatogm24 <renato.garay@pucp.pe>
2022-01-31 23:15:09 -0500

    logging

--- server.py
+++ server.py
@@ -3,4 +3,6 @@ from flask_app.controllers import recipes
 from flask_app import app
 
 if __name__ == "__main__":
+    import logging
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
     app.run(debug=True)
